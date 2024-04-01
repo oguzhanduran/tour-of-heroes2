657bb70d433e7d84e894548cf65f25687e68ec02
`Oguzhan <oguzhan_duran@epam.com>
2024-04-01 15:42:33 +0300

    added lazy loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,8 +4,19 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-  {'path': 'heroes', component: HeroesComponent},
-  {'path': 'dashboard', component: DashboardComponent},
-  {'path': 'detail/:id', component: HeroDetailComponent},
+  {
+    path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  {
+    'path': 'heroes',
+    loadComponent: () => import('./heroes/heroes.component').then(c => c.HeroesComponent)
+  },
+  {
+    'path': 'dashboard',
+    loadComponent: () => import('./dashboard/dashboard.component').then(c => c.DashboardComponent)
+  },
+  {
+    'path': 'detail/:id',
+    loadComponent: () => import('./hero-detail/hero-detail.component').then(c => c.HeroDetailComponent)
+  },
+
 ];
